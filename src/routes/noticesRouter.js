d3990cc6fbf2cde0f36d9251cfbf54d674457984
Olena Bondar <olena.bondar1060@gmail.com>
2023-05-11 20:12:40 +0300

    Update noticesRouter.js

--- src/routes/noticesRouter.js
+++ src/routes/noticesRouter.js
@@ -1,7 +1,25 @@
 const express = require("express");
-const noticesRouter = express.Router();
+const router = express.Router();
 
-// const { noticesController } = require("../../controllers");
+// const { noticesControllers } = require("../../controllers");
 // const { noticesMiddlewares, userMiddlewares } = require("../../middlewares");
+const { validateBody } = require("../utils/validateBody");
+const { uploadCloud } = require("../middlewares");
+const {
+  createNoticeSchema,
+} = require("../utils/validation/noticesValidationSchemas");
+const {
+  getNotices,
+  createNotice,
+} = require("../controllers/noticesControllers");
 
-module.exports = noticesRouter;
+router.get("/", getNotices);
+
+router.post(
+  "/",
+  // uploadCloud.single("avatars"),
+  validateBody(createNoticeSchema),
+  createNotice
+);
+
+module.exports = { noticesRouter: router };
