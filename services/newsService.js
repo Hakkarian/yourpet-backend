221b9a70f03d5a621d2c0bc903d022a124b7728e
Hakkar <zugrutus@gmail.com>
2023-05-19 19:06:43 +0100

    news-fix

--- services/newsService.js
+++ services/newsService.js
@@ -1,17 +1,17 @@
 const { News } = require("../models");
 
-exports.getNews = async ({ skip = 0, limit = 6 }) => {
-  try {
-    const total = await News.find().count();
-    const news = await News.find().skip(skip).limit(limit);
+// exports.getNews = async ({ skip = 0, limit = 6 }) => {
+//   try {
+//     const total = await News.find().count();
+//     const news = await News.find().skip(skip).limit(limit);
 
-    return { news, total };
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return { news, total };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-exports.getNewsByTitle = async ({ search }, { skip = 0, limit = 6 }) => {
+exports.getNews = async ({ search }, { skip = 0, limit = 6 }) => {
   try {
     const total = await News.find({
       title: { $regex: new RegExp(search, "i") },
